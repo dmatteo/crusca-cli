e9ccd3221bb2686d2f34ca62301be23af47662a1
Domenico Matteo <matteo.domenico@gmail.com>
2016-03-02 13:16:40 +0100

    CLI-interface: define required and optional args, help and examples

---
+++ src/cli-interface.js
@@ -0,0 +1,42 @@
+/**
+ * Client Interface configuration
+ * @type {{argv: module.exports.argv}}
+ */
+module.exports = {
+  argv: () => {
+    return require('yargs')
+      .usage('Usage: $0 -i input-file-or-dir -o output-file')
+
+      .example('$0 -i path/to/js -o project.pot',
+        'Recursively scan the directory and parse JS files to look for strings')
+      .example('$0 -i file.js -o project.pot',
+        'parse single Javascript file to look for strings')
+
+      .demand('i')
+      .alias('i', 'in')
+      .nargs('i', 1)
+      .describe('i', 'File or directory to search for translation strings')
+
+      .demand('o')
+      .alias('o', 'out')
+      .nargs('o', 1)
+      .describe('o', 'Output file that will contain the extracted strings')
+
+      .alias('e', 'ext')
+      .describe('e', 'Whitelist file extensions you want to parse (default .js)\nNOT SUPPORTED YET, use .cruscarc')
+
+      .alias('g', 'ignore')
+      .describe('g', 'Blacklist file or directories you don\'t want to scan\nNOT SUPPORTED YET, use .cruscarc')
+
+      .alias('c', 'config')
+      .describe('c', 'Custom config path (default ./cruscarc)')
+
+      .alias('v', 'verbose')
+      .describe('v', 'Enable chatty version')
+
+      .help('h')
+      .alias('h', 'help')
+
+      .argv;
+  }
+};
