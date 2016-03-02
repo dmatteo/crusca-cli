19ebf580ad0ae3825c11a91a4e7b47e061c16750
Domenico Matteo <matteo.domenico@gmail.com>
2016-03-02 13:21:34 +0100

    Crusca-CLI: main source file

---
+++ src/crusca-cli.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const crusca = require('crusca');
+const Promise = require('bluebird');
+const fs = require('graceful-fs');
+const path = require('path');
+const recursive = require('recursive-readdir');
+const readFile = Promise.promisify(fs.readFile);
+const writeFile = Promise.promisify(fs.writeFile);
+
+const argv = require('./cli-interface').argv();
+
+const filePath = argv.in;
+const outFile = argv.out;
+const verbose = argv.verbose;
+
+// Default should go in a separate place
+const DEFAULT_CONFIG = './.cruscarc';
+const DEFAULT_EXTENSION = 'js';
+const DEFAULT_IGNORE = [];
+
+const isFile = fs.lstatSync(filePath).isFile();
+
+const getConfig = () => {
+  const configFile = argv.config || DEFAULT_CONFIG;
+
+  try {
+    fs.lstatSync(configFile);
+  } catch(err) {
+    return {};
+  }
+
+  return JSON.parse(
+    fs.readFileSync(configFile, 'utf8')
+  );
+};
+
+const processFile = (data, filePath, pKeys) => {
+  const keys = crusca.extract(data);
+  return crusca.tagKeys(keys, filePath, pKeys);
+};
+
+if (isFile) {
+
+  readFile(filePath, 'utf8').then((data) => {
+    const taggedKeys = processFile(data, filePath);
+    const keysCount = Object.keys(taggedKeys).length;
+
+    writeFile(outFile, crusca.generatePot(taggedKeys)).then(() => {
+      console.log(`${keysCount} strings extracted from ${filePath}`);
+      process.exit(0);
+    }, (err) => {
+      throw new Error(err);
+    });
+  });
+} else {
+
+  const config = getConfig();
+
+  const ignoreArr = config.ignore || [DEFAULT_IGNORE];
+  const extensions = config.extensions || [DEFAULT_EXTENSION];
+  const whitelistExts = extensions.map(el => `.${el}`);
+  const whitelistFunc = (file, stats) => {
+    return stats.isFile() ? whitelistExts.indexOf(path.extname(file)) === -1 : false;
+  };
+
+  recursive(filePath, ignoreArr.concat(whitelistFunc), (err, files) => {
+    const fileContentArray = files.map((file) => readFile(file, 'utf8'));
+    const filesCount = fileContentArray.length;
+
+    Promise.all(fileContentArray).then((data) => {
+      const taggedKeys = data.reduce((keys, data, idx) => {
+        if (verbose) console.log(files[idx]);
+        return processFile(data, files[idx], keys)
+      }, {});
+
+      const keysCount = Object.keys(taggedKeys).length;
+      const potFileContent = crusca.generatePot(taggedKeys);
+
+      writeFile(outFile, potFileContent).then(() => {
+        console.log(`${keysCount} strings extracted from ${filesCount} files.`);
+        process.exit(0);
+      }, (err) => {
+        throw new Error(err);
+      });
+    });
+  });
+}
