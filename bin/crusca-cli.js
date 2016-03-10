b16a2cc4ddd50455eb4e43e316886a8fa1459983
Domenico Matteo <matteo.domenico@gmail.com>
2016-03-09 20:23:16 -0500

    bin: requires cli and arguments, and starts the client with those

--- bin/crusca-cli.js
+++ bin/crusca-cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+'use strict';
 
-require("../lib/crusca-cli");
+// get the client
+var cli = require("../lib/crusca-cli");
+
+// get the cli arguments
+var argv = require('../lib/cli-interface').argv();
+
+// run the client with the given arguments
+cli.default(argv);
